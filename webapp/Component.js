00948ef387e4a43cf761a074886f2b636502947c
GerardMasdeu <gerard.masdeu2000@gmail.com>
2026-02-23 17:30:38 +0100

    UI5 up and Running

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,17 +1,15 @@
-sap.ui.define([
-    "sap/ui/core/UIComponent"
-], function (UIComponent) {
-    "use strict";
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  "use strict";
 
-    return UIComponent.extend("my.filmSimulation.library.Component", {
-        metadata: {
-            manifest: "json"
-        },
-        init: function () {
-            // call the base component's init function
-            UIComponent.prototype.init.apply(this, arguments);
-            // enable routing if you set it up in manifest
-            this.getRouter().initialize();
-        }
-    });
+  return UIComponent.extend("my.filmSimulation.library.Component", {
+    metadata: {
+      manifest: "json",
+    },
+    init: function () {
+      // call the base component's init function
+      UIComponent.prototype.init.apply(this, arguments);
+      // enable routing if you set it up in manifest
+      // this.getRouter().initialize();
+    },
+  });
 });
